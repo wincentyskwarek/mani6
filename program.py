6661c6c24eb2c4a1aed3ba5e0de54ca3977d2e55
Wincenty Skwarek <wincentyskwarek@op.pl>
2025-04-03 16:56:41 +0200

    proba

--- program.py
+++ program.py
@@ -7,11 +7,14 @@ PUL_PIN1 = 5   # PUL - sygnał kroków
 DIR_PIN1 = 6   # DIR - kierunek obrotu
 PUL_PIN2 = 17   # PUL - sygnał kroków
 DIR_PIN2 = 27   # DIR - kierunek obrotu
+PUL_PIN3 = 23   # PUL - sygnał kroków
+DIR_PIN3 = 24   # DIR - kierunek obrotu
+
 # Ilość kroków do wykonania
-steps = 5000
+steps = 3000
 
 # Czas trwania impulsu (szybkość obrotu) – im krótszy, tym szybciej
-pulse_delay = 0.0005  # 1 ms
+pulse_delay = 0.001  # 1 ms
 
 # Ustawienie trybu pinów
 GPIO.setmode(GPIO.BCM)
@@ -20,18 +23,32 @@ GPIO.setup(PUL_PIN1, GPIO.OUT)
 GPIO.setup(DIR_PIN1, GPIO.OUT)
 GPIO.setup(PUL_PIN2, GPIO.OUT)
 GPIO.setup(DIR_PIN2, GPIO.OUT)
+GPIO.setup(PUL_PIN3, GPIO.OUT)
+GPIO.setup(DIR_PIN3, GPIO.OUT)
 
 try:
     # Ustaw kierunek (True = do przodu, False = do tyłu)
     GPIO.output(DIR_PIN1, False)
     GPIO.output(DIR_PIN2, False)
+    GPIO.output(DIR_PIN3, False)
 
     # Generuj impulsy
+    steps=1000
     for _ in range(steps):
         GPIO.output(PUL_PIN1, GPIO.HIGH)
         time.sleep(pulse_delay)
         GPIO.output(PUL_PIN1, GPIO.LOW)
         time.sleep(pulse_delay)
+
+    steps = 1000
+
+    for _ in range(steps):
+        GPIO.output(PUL_PIN2, GPIO.HIGH)
+        time.sleep(pulse_delay)
+        GPIO.output(PUL_PIN2, GPIO.LOW)
+        time.sleep(pulse_delay)
+    steps = 1000
+
     for _ in range(steps):
         GPIO.output(PUL_PIN2, GPIO.HIGH)
         time.sleep(pulse_delay)
@@ -41,3 +58,4 @@ try:
 
 finally:
     GPIO.cleanup()
+
